fc29dbe54556cf13e1398ef16078acac101d882f
Gökhan Duman <>
2021-06-09 23:11:34 +0300

    added angular material modules to auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // FORM MODÜLLERİ
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+// ANGULAR MATERIAL MODULLERİ
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { AuthRoutingModule } from './auth-routing.module';
 // LAYOUT BİLEŞENİ
 import { AuthComponent } from './auth.component';
@@ -12,7 +16,17 @@ import { LoginComponent } from './contents/login/login.component';
 
 @NgModule({
   declarations: [AuthComponent, RegisterComponent, LoginComponent],
-  // FORM MODÜLLERİNİ DAHİL ETMEK
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, AuthRoutingModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    // FORM MODÜLLERİ
+    FormsModule,
+    ReactiveFormsModule,
+    // MATERIAL MODULLER
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
 export class AuthModule {}
